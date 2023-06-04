dbf47cde65721e30006b93de7d36c73119578b00
mazurkevych30 <132198562+mazurkevych30@users.noreply.github.com>
2023-06-05 00:08:36 +0100

    Summary

--- js/modal.js
+++ js/modal.js
@@ -5,8 +5,7 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.openModalBtn.addEventListener("click", () => console.log(123));
+  refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
